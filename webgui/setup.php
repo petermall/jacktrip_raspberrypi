b6e2be563464c9f45dae832dcf729b98f6b0dbdd
petermall <info@petermall.de>
2020-12-09 23:39:46 +0100

    edit

--- webgui/setup.php
+++ webgui/setup.php
@@ -1,16 +1,21 @@
 <?php
 
 
-if (isset($_POST['volume'])) {
-        $vol = $_POST["volume"];
-        exec("sudo amixer -c 1 sset ADC $vol 2>&1", $out);
+JACKAUDIO =
 
+exec("sudo echo $JACKAUDIO > /etc/jacktrip/jackconf")
+exec("sudo echo $JACKTRIP >> /etc/jacktrip/jackconf")
 
+//To be
+//_OPTS=-t -z -n 2 -C server --bindport 4494 --clientname hubserver -q 2
+//S=-R -dalsa -dhw:sndrpihifiberry --rate 44100 --period 128
+
+23:37:46.8
 //echo "Test <br>";
-        exec('sudo amixer -c 1 sget ADC 2>&1', $output);
-        var_dump($output[4]);
-        echo "<br>";
-        var_dump($output[5]);
+//        exec('sudo amixer -c 1 sget ADC 2>&1', $output);
+//        var_dump($output[4]);
+//        echo "<br>";
+//        var_dump($output[5]);
 }
 //echo $_POST["volume"];
 
@@ -21,12 +26,13 @@ if (isset($_POST['volume'])) {
 <h1>Jackaudio und JackTrip Setup</h1>
 
 <form action="" method="post">
-Sample rate (44100, 48000): <input type="text" name="rate", value="44100">
-period (64,128,256,1024): <input type="text" name="period", value="128">
-Server Name: <input type="text" name="server">
-Queue (2+, auto): <input type="text" name="queue", value="3">
-Bindport: <input type="text" name="bindport", value="4464">
-Number of channes: <input type="text" name="num", value="2">
+Sample rate (44100, 48000): <input type="text" name="rate", value="44100"><br>
+period (64,128,256,1024): <input type="text" name="period", value="128"><br>
+Server Name: <input type="text" name="server"><br>
+Queue (2+, auto): <input type="text" name="queue", value="3"><br>
+Bindport: <input type="text" name="bindport", value="4464"><br>
+Number of channes: <input type="text" name="num", value="2"><br>
+
 
 
 <input type="submit">
