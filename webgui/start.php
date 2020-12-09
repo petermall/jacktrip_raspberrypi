d76565afe3a0d0bd0cbae55e09bdfefc82a31a86
petermall <info@petermall.de>
2020-12-10 00:22:25 +0100

    edit

--- webgui/start.php
+++ webgui/start.php
@@ -2,9 +2,9 @@
 echo "<h1>Jacktrip wird gestartet</h1>";
 echo "\n";
 exec('sudo /bin/systemctl start jack.service');
-exec('sudo /bin/systemctl start jacktrip.service');
+exec('sudo /bin/systemctl start jacktrip.service', $jacktrip, $return);
 echo "Gestartet\n";
+var_dump($jacktrip);
 ?>
 <br>
 <a href="index.html">Zurueck</a>
-
