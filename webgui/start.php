2845e680182323f9da0fe8faf9735920c03f8b9a
Peter Mall <mail@petermall.de>
2020-12-17 16:09:13 +0100

    JMESS

--- webgui/start.php
+++ webgui/start.php
@@ -3,6 +3,10 @@ echo "<h1>Jacktrip wird gestartet</h1>";
 echo "\n";
 exec('sudo /bin/systemctl start jack.service');
 exec('sudo /bin/systemctl start jacktrip.service', $jacktrip, $return);
+exec('sudo su jacktrip -c "/usr/bin/jmess -D -c /var/tmp/test.xml"', $jmess, $jreturn);
+
+echo "Jmess ok? ";
+echo $jreturn;
 
 echo "Gestartet\n";
 echo $return;
